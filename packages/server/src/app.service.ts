d0ef47b4d12e7cd8a3edbf370cb5b9bad1126353
Ross Reicks <rreicks13@gmail.com>
2025-11-25 22:45:40 -0600

    add some build steps

--- packages/server/src/app.service.ts
+++ packages/server/src/app.service.ts
@@ -4,6 +4,6 @@ import { Example } from "@shared/types/example";
 @Injectable()
 export class AppService {
 	async getHello(): Promise<Example> {
-		return await Promise.resolve({ name: "John", age: 30 });
+		return await Promise.resolve({ name: "John", age: 31 });
 	}
 }
